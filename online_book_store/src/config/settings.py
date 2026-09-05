27475472bafd04a600168e3be973db57449d330f
MohammedFathi <mohmmedfathi.123@gmail.com>
2026-09-05 16:24:51 +0300

    add custom user model

--- online_book_store/src/config/settings.py
+++ online_book_store/src/config/settings.py
@@ -80,6 +80,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+AUTH_USER_MODEL = "users.User"
+
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
