655bcbabb189a2a7e173a071c37f8a0f955f1d2c
MohammedFathi <mohmmedfathi.123@gmail.com>
2026-09-06 10:24:09 +0300

    use simplejwt views for login and refresh

--- online_book_store/src/users/api.py
+++ online_book_store/src/users/api.py
@@ -1,53 +1,9 @@
-from rest_framework import status
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework_simplejwt.authentication import AUTH_HEADER_TYPES
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-from rest_framework_simplejwt.serializers import (
-    TokenObtainPairSerializer,
-    TokenRefreshSerializer,
-)
 
 from .serializers import RegisterSerializer
 
 
-class RegisterView(APIView):
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
-
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class LoginView(APIView):
-    authentication_classes = []
-    permission_classes = [AllowAny]
-    www_authenticate_realm = "api"
-
-    def get_authenticate_header(self, request):
-        return f'{AUTH_HEADER_TYPES[0]} realm="{self.www_authenticate_realm}"'
-
-    def post(self, request):
-        serializer = TokenObtainPairSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.validated_data)
-
-
-class RefreshView(APIView):
-    authentication_classes = []
-    permission_classes = [AllowAny]
-    www_authenticate_realm = "api"
-
-    def get_authenticate_header(self, request):
-        return f'{AUTH_HEADER_TYPES[0]} realm="{self.www_authenticate_realm}"'
-
-    def post(self, request):
-        serializer = TokenRefreshSerializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except TokenError as error:
-            raise InvalidToken(error.args[0]) from error
-        return Response(serializer.validated_data)
--- online_book_store/src/users/urls.py
+++ online_book_store/src/users/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenBlacklistView
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-from .api import LoginView, RefreshView, RegisterView
+from .api import RegisterView
 
 urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
-    path("auth/login/", LoginView.as_view(), name="login"),
-    path("auth/refresh/", RefreshView.as_view(), name="refresh"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("auth/logout/", TokenBlacklistView.as_view(), name="logout"),
 ]
